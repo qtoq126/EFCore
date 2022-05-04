425439b0bbda72a7ed87f1456ffa0f5792dc48bd
qtoq126 <qtoq126@gmai.com>
2022-05-04 16:23:05 +0800

    test commit

---
+++ EFDemo.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFDemo.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EFDemo.Domain/Club.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFDemo.Domain
+{
+    public class Club
+    {
+        public Club()
+        {
+            Players = new List<Player>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateOfEstablishment { get; set; }
+
+        public string History { get; set; }
+
+        public League League { get; set; }
+
+        public List<Player> Players { get; set; }
+    }
+}
